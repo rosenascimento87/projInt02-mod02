7f5bdff55f46abdc3e699ddc76d37e4b0da7983c
Rose Nascimento <rosepael.tec@gmail.com>
2025-06-05 06:04:02 -0300

    Atualização dos Models do projeto

--- backend/models/animalModel.js
+++ backend/models/animalModel.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
+const AnimalImageSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  isPrimary: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  _id: false
+});
+
 const AnimalSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -7,8 +21,12 @@ const AnimalSchema = new mongoose.Schema({
     trim: true
   },
   species: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Species',
+    required: true
+  },
+  breed: {
     type: String,
-    required: true,
     trim: true
   },
   age: {
@@ -24,6 +42,37 @@ const AnimalSchema = new mongoose.Schema({
     type: Number,
     min: 0
   },
+  gender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Gender'
+  },
+  size: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Size'
+  },
+  status: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Status',
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  images: {
+    type: [AnimalImageSchema],
+    default: []
+  },
+  firstImageUrl: {
+    type: String,
+    trim: true
+  },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
@@ -33,6 +82,14 @@ const AnimalSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+AnimalSchema.pre('save', function(next) {
+  if (this.images && this.images.length > 0) {
+    const primaryImage = this.images.find(img => img.isPrimary) || this.images[0];
+    this.firstImageUrl = primaryImage.url;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Animal', AnimalSchema);
---
+++ backend/models/speciesModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const SpeciesSchema = new mongoose.Schema({
+  value: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  descricao: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  icon: {
+    type: String,
+    trim: true,
+    default: null
+  }
+}, {
+  timestamps: true,
+  versionKey: false
+});
+
+module.exports = mongoose.model('Species', SpeciesSchema);
